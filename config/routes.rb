51aa6833f6abeb4f7cc3df695a7b7f2c75f0a5b2
Ubuntu <coh46r@gmail.com>
2017-09-16 10:20:44 +0000

    Add bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     end
   end
 
-  get 'pictures' => 'pictures#index'
+  root 'top#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
